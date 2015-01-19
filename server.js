9c2f9c9bea731bd37fec9bc30f81d952e4ac2d55
rfkdali <rfkdali@gmail.com>
2015-01-19 19:23:28 +0100

    update function tableauPower to return a JSON

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require('express');
 
 var app = express();
 
-
-
 app.get('/', function(req, res) {
 	res.setHeader('Content-Type', 'text/html');
 	res.render('index.ejs');
@@ -15,6 +13,33 @@ app.get('/', function(req, res) {
 .get('/ping', function(req, res) {
 	res.setHeader('Content-Type', 'application/json');
 	res.send('{"alive" : true}');
+})
+.get('/primeFactors', function(req, res) {
+
+	function tableauPower(){
+	//on declare number comme la valeur du parametre number de l'url
+	var number = req.param("number");
+	var init = 1;
+	tab = new Array;
+	if(number % 2 == 0){
+		for(i=0; init<number; i++){
+			init *= 2;
+			tab[i] = 2;
+		}
+	}
+	if(init == number){
+		res.setHeader('Content-Type', 'application/json');
+    	res.end(JSON.stringify({ "number" : number, "decomposition" : tab  }));
+
+	}
+	else{
+		res.send("not a power of 2")
+	}
+}
+
+tableauPower();
+
+
 });
 
 
